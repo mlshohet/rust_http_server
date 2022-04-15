50c7d9da45c22ba85b3db004e89a9a70516dc988
SHOHET, MICHAEL <michaelshohet@northwesternmutual.com>
2022-04-15 15:29:24 -0400

    added custom errors and buffer reader

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,8 @@
 use std::io::Read;
+// crate keyword means the root of the entire crate
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -31,6 +35,13 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            
+                            // creates a slice contains entire array
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
+                            let res: &Result<Request, _> = &buffer[..].try_into();
                         },
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
