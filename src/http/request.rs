c109575af2f3c500808073c85335569975f8c700
SHOHET, MICHAEL <mlshohet@gmail.com>
2022-06-05 14:31:13 -0400

    change error name back

--- src/http/request.rs
+++ src/http/request.rs
@@ -71,7 +71,7 @@ impl ParseError {
         match self {
             Self::InvalidRequest => "Invalid Request",
             Self::InvalidEncoding => "Invalid Encoding",
-            Self::InvalidProtocol => "Invalid Protocl",
+            Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method",
         }
     }
